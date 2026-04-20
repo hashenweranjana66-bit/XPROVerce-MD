9bb6dc81fefd78c9ae0cac6efc613e9d22ddb8ba
hashenweranjana66-bit <hashenweranjana66@gmail.com>
2026-04-20 19:46:52 +0530

    Update session.js

--- session.js
+++ session.js
@@ -37,6 +37,6 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "your session",
+SESSION_ID: process.env.SESSION_ID || "XPRO-MD~1774324926558_9hegfoixu",
 PORT: process.env.PORT || "8000"
 };
